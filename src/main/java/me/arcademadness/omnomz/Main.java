f45ff439bb21a5d98c91038e671c03218bd175ca
TrademarkTHIS <jaketvro@gmail.com>
2023-07-17 13:51:22 -0700

    Fixed targeting of players in creative mode.

--- src/main/java/me/arcademadness/omnomz/Main.java
+++ src/main/java/me/arcademadness/omnomz/Main.java
@@ -6,7 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
-public final class Omnomz extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
