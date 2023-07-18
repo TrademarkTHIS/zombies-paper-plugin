3c0a30c8a9f20cff6999ea92101614100b73d392
ZombieSlayerTv <zwhelms@gmail.com>
2023-07-17 21:44:07 -0500

    added zsniper
    Signed-off-by: ZombieSlayerTv <zwhelms@gmail.com>

--- src/main/java/me/arcademadness/omnomz/SniperCommand.java
+++ src/main/java/me/arcademadness/omnomz/SniperCommand.java
@@ -1,5 +1,7 @@
 package me.arcademadness.omnomz;
 
+import io.papermc.paper.event.player.AsyncChatCommandDecorateEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -8,6 +10,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class SniperCommand implements CommandExecutor {
 
@@ -16,8 +21,17 @@ public class SniperCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player p = (Player) sender;
             p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+
             ItemStack sniper = new ItemStack(Material.TRIDENT, 1);
+            ItemMeta smeta = sniper.getItemMeta();
+
+            smeta.setDisplayName("Zombie Sniper");
+            sniper.setItemMeta(smeta);
+
             sniper.addEnchantment(Enchantment.LOYALTY, 3);
+            sniper.addEnchantment(Enchantment.CHANNELING, 1);
+            sniper.addEnchantment(Enchantment.VANISHING_CURSE, 1);
+
             p.getInventory().addItem(sniper);
             p.sendMessage("Zombie Sniper Summoned!");
             return true;
