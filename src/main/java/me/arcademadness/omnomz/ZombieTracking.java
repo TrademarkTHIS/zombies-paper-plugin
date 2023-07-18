c3aa86ab3556777f2d109bf58ca6d354cf4cbbdc
TrademarkTHIS <jaketvro@gmail.com>
2023-07-17 21:03:28 -0700

    Fixed many issues and introduced a 5-second timer for when a player can alert zombies. Should reduce lag, didn't notice a difference in TPS, but client is stuttering with high quantities of zombies...

--- src/main/java/me/arcademadness/omnomz/ZombieTracking.java
+++ src/main/java/me/arcademadness/omnomz/ZombieTracking.java
@@ -1,6 +1,5 @@
 package me.arcademadness.omnomz;
 
-import org.bukkit.Bukkit;
 import org.bukkit.GameEvent;
 import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
@@ -14,50 +13,55 @@ import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.world.GenericGameEvent;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class ZombieTracking implements Listener {
 
     private static final EntityType mobs[] = {EntityType.ZOMBIE, EntityType.HUSK, EntityType.DROWNED, EntityType.ZOMBIE_VILLAGER};
+    HashMap<Player, Instant> lastAlert = new HashMap<>();
+
 
     @EventHandler
     public void onSound(GenericGameEvent event) {
         if (event.getEntity() != null) {
+            if (event.getEntity().getType() != EntityType.PLAYER) return;
             double r = 256;
 
-            if (event.getEntity().getType() == EntityType.PLAYER) {
-                Player p = (Player) event.getEntity();
+            Player p = (Player) event.getEntity();
 
-                //this first one is an example of how we can tweak when zombies chase us
-                //the rest are events that don't make sense to trigger a zombie
-                if (!p.isSprinting())
-                    if (event.getEvent() == GameEvent.STEP)
-                        return;
-                if (event.getEvent() == GameEvent.ELYTRA_GLIDE)
-                    return;
-                if (event.getEvent() == GameEvent.HIT_GROUND)
-                    return;
-                if (event.getEvent() == GameEvent.SWIM)
-                    return;
-                if (event.getEvent() == GameEvent.ITEM_INTERACT_START)
-                    return;
-                if (event.getEvent() == GameEvent.ITEM_INTERACT_FINISH)
-                    return;
+            if (lastAlert.get(p) != null) {
+                if (Duration.between(lastAlert.get(p), Instant.now()).toMillis() < 5000) return;
+            }
 
-                //Don't run if the player is in creative/spectator
-                if (p.getGameMode() == GameMode.SPECTATOR)
-                    return;
+            if (event.getEvent() == GameEvent.STEP)
+                return;
+            if (event.getEvent() == GameEvent.ELYTRA_GLIDE)
+                return;
+            if (event.getEvent() == GameEvent.HIT_GROUND)
+                return;
+            if (event.getEvent() == GameEvent.SWIM)
+                return;
+            if (event.getEvent() == GameEvent.ITEM_INTERACT_START)
+                return;
+            if (event.getEvent() == GameEvent.ITEM_INTERACT_FINISH)
+                return;
 
-                List<Entity> entities = p.getNearbyEntities(r, r, r);
-                for (Entity e : entities) {
-                    if (Arrays.asList(mobs).contains(e.getType())) {
+            //Don't run if the player is in creative/spectator
+            if (p.getGameMode() == GameMode.SPECTATOR)
+                return;
+
+            lastAlert.put(p, Instant.now());
+            List<Entity> entities = p.getNearbyEntities(r, r, r);
+            for (Entity e : entities) {
+                if (Arrays.asList(mobs).contains(e.getType())) {
+                    Zombie z = (Zombie) e;
+                    if (z.getTarget() == null) {
                         if (e.getLocation().y() >= (p.getLocation().y() - 10) && e.getLocation().y() <= (p.getLocation().y() + 10)) {
-                            Zombie z = (Zombie) e;
-                            if (z.getTarget() == null) {
-                                z.setTarget(p);
-                            }
+                            z.setTarget(p);
                         }
                     }
                 }
