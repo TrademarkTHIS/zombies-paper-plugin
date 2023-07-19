3a72719b479442ed367a5162663ef905dc9833a1
ZombieSlayerTv <zwhelms@gmail.com>
2023-07-18 21:43:52 -0500

    Updated HelpCommand
    Signed-off-by: ZombieSlayerTv <zwhelms@gmail.com>

--- src/main/java/me/arcademadness/omnomz/HelpCommand.java
+++ src/main/java/me/arcademadness/omnomz/HelpCommand.java
@@ -3,8 +3,8 @@ package me.arcademadness.omnomz;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 public class HelpCommand implements CommandExecutor {
 
@@ -14,7 +14,10 @@ public class HelpCommand implements CommandExecutor {
         if(sender instanceof Player) {
             Player p = (Player) sender;
 
-            p.sendMessage("Zombie Help Menu :) ");
+            p.sendMessage(ChatColor.GRAY + "-----Zombie Help Menu!-----" + "\n"
+                    + ChatColor.GREEN + "/Zombies" + ChatColor.GRAY + ": Displays this menu. :)" + "\n"
+                    + ChatColor.GREEN + "/ZSummon [<AMOUNT>]" + ChatColor.GRAY + ": Summons a specific number of zombies at your location." + "\n"
+                    + ChatColor.GREEN + "/ZSniper" + ChatColor.GRAY + ": Gives you a trident with mystical undead powers." + "\n");
             return true;
         }
         return false;
