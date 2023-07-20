4439ac64c75eef9733d29fc8d50ba6e956e0d132
TrademarkTHIS <jaketvro@gmail.com>
2023-07-20 01:09:03 -0700

    CLEANING

--- src/main/java/me/arcademadness/omnomz/MakeZombieJump.java
+++ src/main/java/me/arcademadness/omnomz/MakeZombieJump.java
@@ -2,6 +2,7 @@ package me.arcademadness.omnomz;
 
 import io.papermc.paper.event.entity.EntityMoveEvent;
 
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
@@ -13,20 +14,30 @@ import java.util.Arrays;
 
 public class MakeZombieJump implements Listener {
 
-    private static final EntityType mobs[] = {EntityType.ZOMBIE, EntityType.HUSK, EntityType.DROWNED, EntityType.ZOMBIE_VILLAGER};
+    private static final EntityType[] mobs = {EntityType.ZOMBIE, EntityType.HUSK, EntityType.DROWNED, EntityType.ZOMBIE_VILLAGER};
 
     @EventHandler
     private void onZombieMove(EntityMoveEvent event) {
         if (!Arrays.asList(mobs).contains(event.getEntity().getType())) return;
         Zombie z = (Zombie) event.getEntity();
         if (z.getTarget() == null) return;
+        if (z.getTarget().getWorld().getEnvironment() != z.getWorld().getEnvironment()) return;
+
+        Location targetLoc = new Location(z.getTarget().getWorld(), z.getTarget().getLocation().x(), 0 , z.getTarget().getLocation().y());
+        Location zombieLoc = new Location(z.getWorld(), z.getLocation().x(), 0 , z.getLocation().y());
+        if (targetLoc.distance(zombieLoc) > 32) {
+            if (!z.hasLineOfSight(z.getTarget().getEyeLocation())) {
+                z.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 5, 4, false, false));
+            }
+        }
+        if (z.getTarget().getLocation().distance(z.getLocation()) < 1.7) {
+            z.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 5, 2, false, false));
+        }
         if (!z.hasLineOfSight(z.getTarget())) {
             if (z.getTarget().getLocation().y() + 1 > z.getLocation().y()) {
-                if (z.getTarget().getWorld().getEnvironment() == z.getWorld().getEnvironment()) {
-                    if (z.getTarget().getLocation().distance(z.getLocation()) <= 6) {
-                        z.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20, 2, false, false));
-                        z.setJumping(true);
-                    }
+                if (z.getTarget().getLocation().distance(z.getLocation()) <= 6) {
+                    z.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20, 2, false, false));
+                    z.setJumping(true);
                 }
             }
         }
--- src/main/java/me/arcademadness/omnomz/ZombieTracking.java
+++ src/main/java/me/arcademadness/omnomz/ZombieTracking.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.world.GenericGameEvent;
 
 import java.time.Duration;
@@ -22,8 +23,8 @@ import java.util.List;
 
 public class ZombieTracking implements Listener {
 
-    private static final EntityType mobs[] = {EntityType.ZOMBIE, EntityType.HUSK, EntityType.DROWNED, EntityType.ZOMBIE_VILLAGER};
-    private static final GameEvent quietEvents[] = new GameEvent[]{
+    private static final EntityType[] mobs = {EntityType.ZOMBIE, EntityType.HUSK, EntityType.DROWNED, EntityType.ZOMBIE_VILLAGER};
+    private static final GameEvent[] quietEvents = new GameEvent[]{
             GameEvent.STEP,
             GameEvent.SWIM,
             GameEvent.ELYTRA_GLIDE,
@@ -33,67 +34,13 @@ public class ZombieTracking implements Listener {
     };
     HashMap<Player, Instant> lastAlert = new HashMap<>();
 
-    public static double radius = 256;
-
-
-    @EventHandler
-    public void onAlert(GenericGameEvent event) {
-        if (event.getEntity() != null) {
-
-            //Not a player? get the fuck out of 'ere...
-            if (event.getEntity().getType() != EntityType.PLAYER) return;
-            Player p = (Player) event.getEntity();
-
-            //Don't run if the player is in creative/spectator.
-            if (p.getGameMode() == GameMode.SPECTATOR || p.getGameMode() == GameMode.CREATIVE) return;
-
-            //Don't run if code was run for player less than 5 seconds ago.
-            //Can be adjusted later if 5 seconds is not long enough.
-            if (lastAlert.get(p) != null) {
-                if (Duration.between(lastAlert.get(p), Instant.now()).toMillis() < 5000) return;
-            }
-
-            if (Arrays.asList(quietEvents).contains(event.getEvent())) {
-                return;
-            }
-            p.sendActionBar(String.valueOf(event.getEvent().getKey()));
-
-            lastAlert.put(p, Instant.now());
-            List<Entity> entities = p.getNearbyEntities(radius, radius, radius);
-            for (Entity e : entities) {
-                if (Arrays.asList(mobs).contains(e.getType())) {
-                    Zombie z = (Zombie) e;
-                    if (e.getLocation().y() >= (p.getLocation().y() - 10) && e.getLocation().y() <= (p.getLocation().y() + 20)) {
-                        if (z.getTarget() == null) {
-                            z.setTarget(p);
-                        } else {
-                            if (z.getLocation().distance(p.getLocation()) < z.getLocation().distance(z.getTarget().getLocation())) {
-                                z.setTarget(p);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @EventHandler
-    public void onDamageAlert(EntityDamageByEntityEvent event) {
-        if (!(event.getDamager() instanceof Player)) return;
-        Player p = (Player) event.getDamager();
-
-        if (lastAlert.get(p) != null) {
-            if (Duration.between(lastAlert.get(p), Instant.now()).toMillis() < 5000) return;
-        }
-
-        p.sendActionBar("damage event");
-
+    private void alert(Player p, double r) {
         lastAlert.put(p, Instant.now());
-        List<Entity> entities = p.getNearbyEntities(radius, radius, radius);
+        List<Entity> entities = p.getNearbyEntities(r, r, r);
         for (Entity e : entities) {
             if (Arrays.asList(mobs).contains(e.getType())) {
                 Zombie z = (Zombie) e;
-                if (e.getLocation().y() >= (p.getLocation().y() - 10) && e.getLocation().y() <= (p.getLocation().y() + 20)) {
+                if ((e.getLocation().y() > 62 && p.getLocation().y() > 62) || (e.getLocation().y() <= 62 && p.getLocation().y() <= 62) || (e.getLocation().y() < p.getLocation().y()+12 && e.getLocation().y() > p.getLocation().y()-12)) {
                     if (z.getTarget() == null) {
                         z.setTarget(p);
                     } else {
@@ -107,12 +54,53 @@ public class ZombieTracking implements Listener {
     }
 
     @EventHandler
+    public void onAlert(GenericGameEvent event) {
+        if (event.getEntity() != null) {
+            double radius = 256;
+
+            if (event.getEntity().getType() != EntityType.PLAYER) return;
+            Player p = (Player) event.getEntity();
+            if (p.getGameMode() == GameMode.SPECTATOR || p.getGameMode() == GameMode.CREATIVE) return;
+            if (lastAlert.get(p) != null) { if (Duration.between(lastAlert.get(p), Instant.now()).toMillis() < 5000) return; }
+            if (Arrays.asList(quietEvents).contains(event.getEvent())) return;
+
+            if (event.getEvent() == GameEvent.SPLASH) radius /= 3;
+
+            p.sendActionBar(String.valueOf(event.getEvent().getKey()));
+
+            this.alert(p, radius);
+        }
+    }
+
+    @EventHandler
+    public void onItemBreak(PlayerItemBreakEvent event) {
+        double radius = 256;
+        Player p = event.getPlayer();
+        this.alert(p, radius);
+    }
+
+    @EventHandler
+    public void onDamageAlert(EntityDamageByEntityEvent event) {
+        double radius = 256;
+        if (!(event.getDamager() instanceof Player)) return;
+        Player p = (Player) event.getDamager();
+
+        if (p.getGameMode() == GameMode.SPECTATOR || p.getGameMode() == GameMode.CREATIVE) return;
+
+        if (lastAlert.get(p) != null) {
+            if (Duration.between(lastAlert.get(p), Instant.now()).toMillis() < 5000) return;
+        }
+
+        this.alert(p, radius);
+    }
+
+    @EventHandler
     public void onUntarget(EntityTargetLivingEntityEvent event) {
         if (event.getReason() == EntityTargetEvent.TargetReason.FORGOT_TARGET || event.getReason() == EntityTargetEvent.TargetReason.UNKNOWN) {
             if (Arrays.asList(mobs).contains(event.getEntity().getType())) {
                 Zombie z = (Zombie) event.getEntity();
                 if (z.getTarget() != null) {
-                    if (z.getTarget().getWorld().getEnvironment() == z.getWorld().getEnvironment()) return;
+                    if (z.getTarget().getWorld().getEnvironment() != z.getWorld().getEnvironment()) return;
                     if (z.getTarget().getType() == EntityType.PLAYER) {
                         Player p = (Player) z.getTarget();
                         if (p.getGameMode() == GameMode.CREATIVE || p.getGameMode() == GameMode.SPECTATOR) return;
